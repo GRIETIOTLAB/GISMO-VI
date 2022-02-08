8081612962623a047c79470bdebdfbda08837fcc
GRIETIOTLAB <77530550+GRIETIOTLAB@users.noreply.github.com>
2022-02-08 15:15:31 +0530

    Update GISMO6_WiFi.ino

--- GettingStarted/GISMO6_WiFi.ino
+++ GettingStarted/GISMO6_WiFi.ino
@@ -3,21 +3,23 @@
 #define WIFI_PASSWORD "xxxxxxxxxxxxx"
 
 void setup() {
+  pinMode(2,OUTPUT);
   Serial.begin(115200);
-  WiFiConnect();
+  WiFiInit();
  }
 
 void loop() {
  
 }
 
-void WiFiConnect(){
+void WiFiInit(){
 //Connecting to Wi-Fi network
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
+    digitalWrite(2,!digitalRead(2));
     delay(300);
   }
   Serial.println();
