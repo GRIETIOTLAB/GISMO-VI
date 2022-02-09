641e5b163d70df2dde24c5810b56eee1cfbd847f
GRIETIOTLAB <77530550+GRIETIOTLAB@users.noreply.github.com>
2022-02-09 10:21:37 +0530

    Update GISMO6_WF_FBW.ino

--- GettingStarted/GISMO6_WF_FBW.ino
+++ GettingStarted/GISMO6_WF_FBW.ino
@@ -1,9 +1,9 @@
 #include <WiFi.h>
 #include "FirebaseESP32.h"
-#define FIREBASE_HOST "radsproject.firebaseio.com" //Do not include https:// in FIREBASE_HOST
-#define FIREBASE_AUTH "4niCZOkFw4C2WVZf04gk9PrQx9isMG7NbCva9CmU"
-#define WIFI_SSID "rads_home"
-#define WIFI_PASSWORD "rads1961"
+#define FIREBASE_HOST "xxxxxxxxxxxxxxxxxxxxxxxxxxxx" //Do not include https:// in FIREBASE_HOST
+#define FIREBASE_AUTH "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define WIFI_SSID "xxxxxxxxxx"
+#define WIFI_PASSWORD "xxxxxxxxxx"
 //Define Firebase Data object
 FirebaseData firebaseData;
 
