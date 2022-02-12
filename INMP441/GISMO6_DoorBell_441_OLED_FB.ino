a2dba6c8411f8c76c49fb128e4ef50893d0de8a6
GRIETIOTLAB <77530550+GRIETIOTLAB@users.noreply.github.com>
2022-02-12 11:06:49 +0530

    Add files via upload

---
+++ INMP441/GISMO6_DoorBell_441_OLED_FB.ino
@@ -0,0 +1,339 @@
+#include <Arduino.h>
+#include <driver/i2s.h>
+#include "arduinoFFT.h"
+#include "Credentials.h"
+
+
+// size of noise sample
+#define SAMPLES 1024
+
+const i2s_port_t I2S_PORT = I2S_NUM_0;
+const int BLOCK_SIZE = SAMPLES;
+
+#define OCTAVES 9
+
+String tag = "IOTLAB/Audio/BellStatus";
+
+// our FFT data
+static float real[SAMPLES];
+static float imag[SAMPLES];
+static arduinoFFT fft(real, imag, SAMPLES, SAMPLES);
+static float energy[OCTAVES];
+// A-weighting curve from 31.5 Hz ... 8000 Hz
+static const float aweighting[] = {-39.4, -26.2, -16.1, -8.6, -3.2, 0.0, 1.2, 1.0, -1.1};
+
+static unsigned int bell = 0;
+static unsigned int fireAlarm = 0;
+static unsigned long ts = millis();
+static unsigned long last = micros();
+static unsigned int sum = 0;
+static unsigned int mn = 9999;
+static unsigned int mx = 0;
+static unsigned int cnt = 0;
+static unsigned long lastTrigger[2] = {0, 0};
+
+void OLEDInit();
+void OLEDUpdate();
+void WiFiinit();
+void FirebaseInit();
+void FirebaseWrite();
+
+
+int bellCount;
+String bellStatus = " No Bell";
+String bellStatus1 = "No_Bell";
+
+static void integerToFloat(int32_t *integer, float *vReal, float *vImag, uint16_t samples)
+{
+    for (uint16_t i = 0; i < samples; i++)
+    {
+        vReal[i] = (integer[i] >> 16) / 10.0;
+        vImag[i] = 0.0;
+    }
+}
+
+// calculates energy from Re and Im parts and places it back in the Re part (Im part is zeroed)
+static void calculateEnergy(float *vReal, float *vImag, uint16_t samples)
+{
+    for (uint16_t i = 0; i < samples; i++)
+    {
+        vReal[i] = sq(vReal[i]) + sq(vImag[i]);
+        vImag[i] = 0.0;
+    }
+}
+
+// sums up energy in bins per octave
+static void sumEnergy(const float *bins, float *energies, int bin_size, int num_octaves)
+{
+    // skip the first bin
+    int bin = bin_size;
+    for (int octave = 0; octave < num_octaves; octave++)
+    {
+        float sum = 0.0;
+        for (int i = 0; i < bin_size; i++)
+        {
+            sum += real[bin++];
+        }
+        energies[octave] = sum;
+        bin_size *= 2;
+    }
+}
+
+static float decibel(float v)
+{
+    return 10.0 * log(v) / log(10);
+}
+
+// converts energy to logaritmic, returns A-weighted sum
+static float calculateLoudness(float *energies, const float *weights, int num_octaves, float scale)
+{
+    float sum = 0.0;
+    for (int i = 0; i < num_octaves; i++)
+    {
+        float energy = scale * energies[i];
+        sum += energy * pow(10, weights[i] / 10.0);
+        energies[i] = decibel(energy);
+    }
+    return decibel(sum);
+}
+
+void setup(void)
+{
+    Serial.begin(115200);
+    Serial.println("Configuring I2S...");
+    esp_err_t err;
+
+    // The I2S config as per the example
+    const i2s_config_t i2s_config = {
+        .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX), // Receive, not transfer
+        .sample_rate = 22627,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT, // for old esp-idf versions use RIGHT
+        .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // Interrupt level 1
+        .dma_buf_count = 8,                       // number of buffers
+        .dma_buf_len = BLOCK_SIZE,                // samples per buffer
+        .use_apll = true};
+
+    // The pin config as per the setup
+    const i2s_pin_config_t pin_config = {
+        .bck_io_num = 14,   // BCKL
+        .ws_io_num = 27,    // LRCL
+        .data_out_num = -1, // not used (only for speakers)
+        .data_in_num = 12   // DOUTESP32-INMP441 wiring
+    };
+
+    // Configuring the I2S driver and pins.
+    // This function must be called before any I2S driver read/write operations.
+    err = i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+    if (err != ESP_OK)
+    {
+        Serial.printf("Failed installing driver: %d\n", err);
+        while (true)
+            ;
+    }
+    err = i2s_set_pin(I2S_PORT, &pin_config);
+    if (err != ESP_OK)
+    {
+        Serial.printf("Failed setting pin: %d\n", err);
+        while (true)
+            ;
+    }
+    Serial.println("I2S driver installed.");
+    OLEDInit();
+    OLEDUpdate();
+    WiFiInit();
+    FirebaseInit();
+}
+
+unsigned int countSetBits(unsigned int n)
+{
+    unsigned int count = 0;
+    while (n)
+    {
+        count += n & 1;
+        n >>= 1;
+    }
+    return count;
+}
+
+//detecting 2 frequencies. Set wide to true to match the previous and next bin as well
+bool detectFrequency(unsigned int *mem, unsigned int minMatch, double peak, unsigned int bin1, unsigned int bin2, bool wide)
+{
+    *mem = *mem << 1;
+    if (peak == bin1 || peak == bin2 || (wide && (peak == bin1 + 1 || peak == bin1 - 1 || peak == bin2 + 1 || peak == bin2 - 1)))
+    {
+        *mem |= 1;
+    }
+
+    if (countSetBits(*mem) >= minMatch)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void sendAlarm(unsigned int index, char *topic, unsigned int timeout)
+{
+    // do not publish if last trigger was earlier than timeout ms
+    if (abs(millis() - lastTrigger[index]) < timeout)
+    {
+        return;
+    }
+
+    lastTrigger[index] = millis();
+    //publish to mqtt
+    //publish(topic, "1");
+}
+
+void sendMetrics(char * topic, unsigned int mn, unsigned int mx, unsigned int avg)
+{
+    String payload = "{\"min\": ";
+    payload += mn;
+    payload += ", \"max\":";
+    payload += mx;
+    payload += ", \"value\":";
+    payload += avg;
+    payload += "}";
+
+    Serial.println(payload);
+    //publish to mqtt
+    //publish(topic, (char *)payload.c_str());
+}
+
+void calculateMetrics(int val) {
+  cnt++;
+  sum += val;
+
+  if (val > mx)
+  {
+      mx = val;
+  }
+
+  if (val < mn)
+  {
+      mn = val;
+  }
+}
+
+void loop(void)
+{
+    if (micros() - last < 45200) {
+      // send mqtt metrics every 10s while waiting and no trigger is detected
+      if (millis() - ts >= 10000 && bell == 0 && fireAlarm == 0)
+      {
+          //Serial.println(cnt[0]);
+          sendMetrics("home/noise", mn, mx, sum / cnt);
+          cnt = 0;
+          sum = 0;
+          mn = 9999;
+          mx = 0;
+          ts = millis();
+         // display.clear();
+         // display.display();
+         bellStatus = "No Bell";
+         bellStatus1 = "No_Bell";
+         OLEDUpdate();
+         FirebaseWrite();
+         Serial.println(bellStatus);
+      }
+      return;
+    }
+
+    last = micros();
+    
+    static int32_t samples[BLOCK_SIZE];
+
+    // Read multiple samples at once and calculate the sound pressure
+    size_t num_bytes_read;
+    esp_err_t err = i2s_read(I2S_PORT,
+                             (char *)samples,
+                             BLOCK_SIZE, // the doc says bytes, but its elements.
+                             &num_bytes_read,
+                             portMAX_DELAY); // no timeout
+    int samples_read = num_bytes_read / 8;
+
+    // integer to float
+    integerToFloat(samples, real, imag, SAMPLES);
+
+    // apply flat top window, optimal for energy calculations
+    fft.Windowing(FFT_WIN_TYP_FLT_TOP, FFT_FORWARD);
+    fft.Compute(FFT_FORWARD);
+
+    // calculate energy in each bin
+    calculateEnergy(real, imag, SAMPLES);
+
+    // sum up energy in bin for each octave
+    sumEnergy(real, energy, 1, OCTAVES);
+
+    // calculate loudness per octave + A weighted loudness
+    float loudness = calculateLoudness(energy, aweighting, OCTAVES, 1.0);
+
+    unsigned int peak = (int)floor(fft.MajorPeak());
+    //Serial.println(peak);
+    
+    // detecting 1kHz and 1.5kHz 738Hz
+    if (detectFrequency(&bell, 15, peak, 33, 34, true))
+    {
+        Serial.println("Detected bell");
+        sendAlarm(0, "home/alarm/doorbell", 2000);
+        bellCount++;
+        if(bellCount > 10){
+          bellStatus = "Bell !!!!";
+          bellStatus1 = "Bell_!_!";
+          OLEDUpdate();
+          FirebaseWrite();
+          Serial.println(bellStatus);
+          bellCount = 0;
+        }
+        
+    }
+    
+
+    //detecting frequencies around 3kHz
+    if (detectFrequency(&fireAlarm, 15, peak, 140, 141, true))
+    {
+        Serial.println("Detected fire alarm");
+        sendAlarm(1, "home/alarm/fire", 10000);
+    }
+
+    calculateMetrics(loudness);
+}
+void OLEDInit(){
+  display.init();
+  display.setFont(ArialMT_Plain_24);
+}
+
+void OLEDUpdate(){
+  display.clear();
+  display.drawString(0,0,bellStatus);
+//  display.drawString(40,30,String(beatAvg));
+  display.display();
+}
+void WiFiInit(){
+  pinMode(2,OUTPUT);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    digitalWrite(2,!digitalRead(2));
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+   
+}
+void FirebaseInit(){
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+}
+void FirebaseWrite(){
+  Firebase.setString(firebaseData,tag,bellStatus1);
+ 
+}
+  
